1a0b160418505f8f8456885aa5520b6ec8c8a13f
albertmolodec <albert.abdu@gmail.com>
2017-11-22 11:01:17 +0500

    Add files via upload

--- EllipticCurve.cpp
+++ EllipticCurve.cpp
@@ -7,43 +7,108 @@
 
 using namespace std;
 
-void get_points(map<int,int> &p, int a, int b, int N)
+int pow(int base, int exp)
 {
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-        {
-            cout << i << " " << j << endl;
-            if ( ((j * j) % N) == ((i * i * i + a * i + b) % N) )
-            {
-                p.insert(pair<int,int> (i,j));
-            }
-        }
+	int result = base;
+	for (int i = 1; i < exp; i++)
+		result *= base;
+	return result;
 }
 
-//int get_lambda()
-
-void print_points(map<int,int> p)
+int mod(int a, int N)
 {
-    map<int,int>::iterator itMap;
+	if (a >= 0)
+		return a % N;
+	else
+		return N - ((-a) % N);
+}
 
-    for (itMap = p.begin(); itMap != p.end(); itMap++)
-        cout << itMap->first << " " << itMap->second << endl;
+int ext_ev(int a, int mod)
+{
+	int q, r, x1, x2, result;
+	x2 = 1, x1 = 0;
+	while (mod > 0)
+	{
+		q = a / mod, r = a % mod;
+		result = x2 - q * x1;
+		a = mod, mod = r;
+		x2 = x1, x1 = result;
+	}
+	if (x2 < 0)
+	{
+		result = x2 + mod;
+	}
+	else
+	{
+		result = x2;
+	}
+	return result;
+}
 
-    cout << "p size = " << p.size() << endl;
+int get_lambda(int x1, int x2, int y1, int y2, int a, int N)
+{
+	if (x1 == x2 && y1 == y2)
+		return mod((3 * x1 * x1 + a) * ext_ev(2 * y1, N), N);
+	else
+		return mod((y2 - y1) * ext_ev(x2 - x1, N), N);
+}
+
+int get_x3(int x1, int x2, int y1, int y2, int a, int N)
+{
+	int lambda = get_lambda(x1, x2, y1, y2, a, N);
+	return mod((pow(lambda, 2) - x1 - x2), N);
+}
+
+int get_y3(int x1, int x2, int y1, int y2, int a, int N)
+{
+	int lambda = get_lambda(x1, x2, y1, y2, a, N);
+	cout << "lambda = " << lambda << endl;
+	return mod((lambda * mod((x1 - x2), N) - y1), N);
+}
+
+void get_points(vector<int> &p, int a, int b, int N)
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (((j * j) % N) == ((i * i * i + a * i + b) % N))
+			{
+				p.push_back(i);
+				p.push_back(j);
+			}
+}
+
+void print_points(vector<int> p)
+{
+	for (int i = 0; i < p.size(); i += 2)
+		cout << "(" << p[i] << "," << p[i + 1] << ")" << endl;
 }
 
 int main()
 {
-    int a = 1;
-    int b = 3;
-    int N = 7;
+	srand(time(NULL));
+	int a = 1;
+	int b = 3;
+	int n = 7;
+	vector<int> p;
+	int nA = 3; // private key of Alice
+	int nB = 5; // private key of Bob
 
-    map <int, int> p;
+	cout << "a = " << a << ", b = " << b << ", N = " << n << endl;
+	
+	get_points(p, a, b, n);
+	cout << "Points:" << endl;
+	print_points(p);
+	cout << "Group order: " << p.size() / 2 + 1 << endl; // +1, because O
 
-    cout << "Points:" << endl;
-    get_points(p,a,b,N);
-    
-    print_points(p);
+	int iG = rand() % (p.size() / 2);
+	cout << "G = (" << p[iG * 2] << ";" << p[iG * 2 + 1] << ")" << endl;
+
+	cout << "Q = (4,1) + (6,6) = (" << get_x3(4, 6, 1, 6, a, n) << ";" << get_y3(4, 6, 1, 6, a, n) << ")" << endl;
 
 
+
+	
+
+
+	system("pause");
 }

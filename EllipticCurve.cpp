0aa392ea204dbc38ab16551c1218f614d16dd6ef
albertmolodec <albert.abdu@gmail.com>
2017-11-22 19:27:49 +0500

    Need to correct function of summ 2 points

--- EllipticCurve.cpp
+++ EllipticCurve.cpp
@@ -23,15 +23,16 @@ int mod(int a, int N)
 		return N - ((-a) % N);
 }
 
-int ext_ev(int a, int mod)
+int ext_ev(int a, int b)
 {
+	int mod = b;
 	int q, r, x1, x2, result;
 	x2 = 1, x1 = 0;
-	while (mod > 0)
+	while (b > 0)
 	{
-		q = a / mod, r = a % mod;
+		q = a / b, r = a % b;
 		result = x2 - q * x1;
-		a = mod, mod = r;
+		a = b, b = r;
 		x2 = x1, x1 = result;
 	}
 	if (x2 < 0)
@@ -48,9 +49,15 @@ int ext_ev(int a, int mod)
 int get_lambda(int x1, int x2, int y1, int y2, int a, int N)
 {
 	if (x1 == x2 && y1 == y2)
+	{
+		// cout << "== " << ext_ev(2*y1, N) << " ";
 		return mod((3 * x1 * x1 + a) * ext_ev(2 * y1, N), N);
+	}
 	else
+	{
+		// cout << "!= " << ext_ev(x2-x1, N) << " ";
 		return mod((y2 - y1) * ext_ev(x2 - x1, N), N);
+	}
 }
 
 int get_x3(int x1, int x2, int y1, int y2, int a, int N)
@@ -62,8 +69,21 @@ int get_x3(int x1, int x2, int y1, int y2, int a, int N)
 int get_y3(int x1, int x2, int y1, int y2, int a, int N)
 {
 	int lambda = get_lambda(x1, x2, y1, y2, a, N);
-	cout << "lambda = " << lambda << endl;
-	return mod((lambda * mod((x1 - x2), N) - y1), N);
+	int x3 = get_x3(x1, x2, y1, y2, a, N);
+	return mod((lambda * mod((x1 - x3), N) - y1), N);
+}
+
+void multi_point(int &result_x, int &result_y, int x, int y, int a, int N, int num)
+{
+	int x3 = x;
+	int y3 = y;
+	for (int i = 0; i < num; i++)
+	{
+		x3 = get_x3(x3, x, y3, y, a, N);
+		y3 = get_y3(x3, x, y3, y, a, N);
+	}
+	result_x = x3;
+	result_y = y3;
 }
 
 void get_points(vector<int> &p, int a, int b, int N)
@@ -88,27 +108,51 @@ int main()
 	srand(time(NULL));
 	int a = 1;
 	int b = 3;
-	int n = 7;
+	int n = 41;
 	vector<int> p;
-	int nA = 3; // private key of Alice
-	int nB = 5; // private key of Bob
-
+	// int nA = rand() % (n-2) + 2;
+	// int nB = rand() % (n-2) + 2;
+	int nA = 3;
+	int nB = 4;
+	int pA_x, pA_y, pB_x, pB_y;
+	
+	cout << "Curve:\ny^2 = x^3 + x + 3 (mod 41)" << endl;
 	cout << "a = " << a << ", b = " << b << ", N = " << n << endl;
-	
+
 	get_points(p, a, b, n);
-	cout << "Points:" << endl;
-	print_points(p);
-	cout << "Group order: " << p.size() / 2 + 1 << endl; // +1, because O
+	// cout << "Points:" << endl;
+	// print_points(p);
+	cout << "Group order: " << p.size() / 2 + 1 << endl << endl ; // +1, because O
+	//int iG = rand() % (p.size() / 2); // genarate random G point
+	int iG = 2;
+	cout << "Generating point G = (" << p[iG * 2] << ", " << p[iG * 2 + 1] << ")" << endl;
 
-	int iG = rand() % (p.size() / 2);
-	cout << "G = (" << p[iG * 2] << ";" << p[iG * 2 + 1] << ")" << endl;
+	cout << "Alice randomly selects private key (nA < N): " << nA << endl;
+	cout << "Bob randomly selects private key (nB < N): " << nB << endl << endl;
 
-	cout << "Q = (4,1) + (6,6) = (" << get_x3(4, 6, 1, 6, a, n) << ";" << get_y3(4, 6, 1, 6, a, n) << ")" << endl;
+	multi_point(pA_x, pA_y, p[iG * 2], p[iG * 2 + 1], a, n, nA);
+	multi_point(pB_x, pB_y, p[iG * 2], p[iG * 2 + 1], a, n, nB);
+ 
+	cout << "Alice calculates her public key: (" << pA_x << ", " << pA_y << ")" << endl;
+	cout << "Bob calculates his public key: (" << pB_x << ", " << pB_y << ")" << endl << endl;
+
+	int KA_x, KA_y, KB_x, KB_y;
+
+	multi_point(KA_x, KA_y, pA_x, pA_y, a, n, nB);
+	multi_point(KB_x, KB_y, pB_x, pB_y, a, n, nB);
+
+	cout << "Alice sends Bob her public key. Bob does the same." << endl;
+	cout << "Alice calculates common secret key: (" << KA_x << ", " << KA_y << ")" << endl;
+	cout << "Bob calculates common secret key: (" << KB_x << ", " << KB_y << ")" << endl << endl;
+
+	if (KA_x == KB_x && KA_y == KB_y)
+		cout << "Keys are the same. Success!" << endl;
+	else
+		cout << "Keys are the different. Wrong!" << endl;
 
 
 
-	
 
-
-	system("pause");
+	cout << get_x3(3,3,19,19,1,41) << endl;
+	cout << get_y3(3,3,19,19,1,41) << endl;
 }

c0f059d21bd4945132beca96ffc63637c7291745
albertmolodec <albert.abdu@gmail.com>
2017-11-27 01:13:13 +0500

    SUCCESS

--- EllipticCurve.cpp
+++ EllipticCurve.cpp
@@ -15,11 +15,9 @@ private:
 	int a;
 	int b;
 	int n;
-	bool is_O;
 
 public:
-	void set_is_O(bool is_O) { this->is_O = is_O; }
-	int get_is_O() { return this->is_O; }
+	bool good;
 
 	void set_xy(int x, int y)
 	{
@@ -41,23 +39,32 @@ public:
 	int get_b() { return this->b; }
 	int get_n() { return this->n; }
 
-	ePoint(int x, int y, int n, bool is_O, int a = 1, int b = 3)
+	ePoint(int x, int y, int n, bool good, int a = 1, int b = 3)
 	{
 		set_xy(x, y);
 		set_n(n);
 		set_ab(a, b);
-		set_is_O(is_O);
+		this->good = good;
 	}
 	ePoint()
 	{
 		set_ab(1, 3);
-		set_is_O(false);
+		set_n(41);
+		this->good = false;
 	}
 	~ePoint() {}
 
 	ePoint operator = (ePoint p1)
 	{
-		return ePoint(this->x = p1.x, this->y = p1.y, this->n = p1.n, this->is_O = p1.is_O, this->a = p1.a, this->b = p1.b);
+		return ePoint(this->x = p1.x, this->y = p1.y, this->n = p1.n, this->good = p1.good, this->a = p1.a, this->b = p1.b);
+	}
+
+	bool operator == (ePoint &p)
+	{
+		if (this->x == p.x && this->y == p.y && this->a == p.a && this->b == p.b && this->n == p.n)
+			return true;
+		else
+			return false;
 	}
 
 	ePoint operator + (ePoint &p2)
@@ -66,7 +73,8 @@ public:
 		/*cout << "plus: ";
 		print_point(*this);
 		print_point(p2);*/
-		int lambda = get_lambda(*this, p2);
+		bool is_good = false;
+		int lambda = get_lambda(*this, p2, is_good);
 		//cout << "lambda = " << lambda << endl;
 
 		int x1 = this->get_x();
@@ -77,7 +85,8 @@ public:
 		p3.set_x( mod(lambda * lambda - x1 - x2, N) );
 		p3.set_y( mod((lambda*(x1-p3.get_x())-y1), N) );
 		p3.set_n(N);
-
+		p3.good = is_good;
+		
 		return p3;
 	}
 
@@ -85,7 +94,9 @@ public:
 	{
 		ePoint p3 = *this;
 		for (int i = 1; i < count; i++)
+		{
 			p3 = p3 + *this;
+		}
 		return p3;
 	}
 
@@ -97,36 +108,38 @@ public:
 			return N - ((-a) % N);
 	}
 
-	int get_lambda(ePoint &p1, ePoint &p2)
+	int get_lambda(ePoint &p1, ePoint &p2, bool &is_good)
 	{
 		int x1 = p1.get_x();
 		int x2 = p2.get_x();
 		int y1 = p1.get_y();
 		int y2 = p2.get_y();
 		int n = p1.get_n();
-		int a = p1.get_a();	
+		int a = p1.get_a();
 		
 		// TO DO: realize function "==" to compare points p1 and p2
 		if (x1 == x2 && y1 == y2)
 		{
 			int inv = ext_ev(2 * y1, n);
-			return (((3 * x1 * x1 + a) * inv) % n);
+			if (2 * y1 == 0)
+			{
+				is_good = true;
+			}
+			return ( mod(((3 * x1 * x1 + a) * inv), n));
 		}
 		else
 		{
 			int inv = ext_ev(x2 - x1, n);
-			return(((y2 - y1) * inv) % n);
+			if (x2 - x1 == 0)
+			{
+				is_good = true;
+			}
+			return ( mod(((y2 - y1) * inv), n));			
 		}
 	}	
 
 	int ext_ev(int a, int b)
 	{
-		if (a == 0)
-		{
-			this->set_is_O(true);
-			cout << get_is_O() << " ";
-		}
-
 		if (a < 0)
 			a = mod(a, b);
 
@@ -148,7 +161,7 @@ public:
 		return result;
 	}
 
-	static void get_points(vector<ePoint> &pts, int a, int b, int n)
+	static void make_points(vector<ePoint> &pts, int a, int b, int n)
 	{
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++)
@@ -168,58 +181,47 @@ public:
 
 	static void print_point(ePoint p)
 	{
-		cout << "(" << p.get_x() << "," << p.get_y() << "). Is O? " << p.get_is_O() << endl;
-		//cout << "(" << p.get_x() << "," << p.get_y() << ") ";
+		cout << "(" << p.get_x() << "," << p.get_y() << ") ";
 	}
 
-	static int get_iG(vector<ePoint> &pts)
+	static int get_iG(vector<ePoint> &pts, int &max_suborder)
 	{
 		int order = pts.size() + 1;
 		int max = 1;
+		vector<int> good_pts;
 
 		for (int i = 1; i < pts.size(); i++)
-			if (order % i == 0)
+			if (order % i == 0 && is_simple(i) == true)
 				max = i;
-
+		max_suborder = max;
+		
 		for (int i = 0; i < pts.size(); i++)
 		{
-			cout << i << endl;
 			ePoint temp = pts[i] * max;
+			if (temp.good == true)
+				good_pts.push_back(i);
 		}
 
-		return max;
+	/*	cout << "Good points: ";
+		for (int i = 0; i < good_pts.size(); i++)
+		{
+			ePoint::print_point(pts[good_pts[i]]);
+		}
+		cout << endl;*/
+
+		int iG = rand() % good_pts.size();
+
+		return good_pts[iG];
 	}
 
-	static void list_O(vector<ePoint> pts)
+	static bool is_simple(int n)
 	{
-		int j = 0;
-		for (int i = 0; i < pts.size(); i++)
-		{
-			if (pts[i].get_is_O() == true)
-			{
-				ePoint::print_point(pts[i]);
-				j++;
-			}
-		}
-		cout << "=== " << j << " ===" << endl;
+		for (int i = 2; i <= sqrt(n); i++)
+			if (n % i == 0)
+				return false;
+		return true;
 	}
 };
-
-bool is_simple(int n)
-{
-	for (int i = 2; i <= sqrt(n); i++)
-		if (n % i == 0)
-			return false;
-	return true;
-}
-
-int pow(int base, int exp)
-{
-	int result = base;
-	for (int i = 1; i < exp; i++)
-		result *= base;
-	return result;
-}
 
 int main()
 {
@@ -227,36 +229,29 @@ int main()
 	int n = 41;
 
 	vector<ePoint> pts;
-	ePoint::get_points(pts, 1, 3, n);
+	
+	ePoint::make_points(pts, 1, 3, n);
 
 	cout << "Points of Elliptic Curve: " << endl;
 	ePoint::print_points(pts);
 
-	//int nA = rand() % (n - 3) + 2;
-	//int nB = rand() % (n - 3) + 2;
-	int nA = 3;
-	int nB = 4;	
 	
-	cout << "Curve:" << endl;
-	cout << "y ^ 2 = x ^ 3 + x + 3 (mod 41)" << endl;
+	
+	cout << endl << "Curve:" << endl;
+	cout << "y^2 = x^3 + x + 3 (mod 41)" << endl;
 	cout << "a = 1, b = 3, N = 41" << endl;
 
 	int group_order = pts.size() + 1;
 	cout << "Group order: " << group_order << endl << endl; // +1, because O  
-	//int iG = rand() % pts.size(); // generate random G point
-	int iG = 2;
 
-	cout << "Generating point G... " << endl;
-	int iG_temp = ePoint::get_iG(pts);
-	cout << "iG = " << iG_temp << ". pts[iG] = ";
-	ePoint::print_point(pts[iG_temp]);
-	ePoint::print_points(pts);
-
-
-	cout << endl << "List of good points: " << endl;
+	int max_suborder;
+	cout << "Generating point G = ";
+	int iG = ePoint::get_iG(pts, max_suborder);
+	ePoint::print_point(pts[iG]);
 	cout << endl;
-	ePoint::list_O(pts);
-
+	
+	int nA = rand() % (max_suborder - 3) + 2;
+	int nB = rand() % (max_suborder - 3) + 2;
 	cout << "Alice randomly selects private key (nA < N): " << nA << endl;
 	cout << "Bob randomly selects private key (nB < N): " << nB << endl << endl;
 
@@ -267,21 +262,22 @@ int main()
 	cout << "Bob calculates his public key: ";
 	ePoint pB = pts[iG] * nB;
 	ePoint::print_point(pB);
+	cout << endl << endl;
+
+	cout << "Alice sends Bob her public key. Bob does the same." << endl;
+	cout << "Alice calculates common secret key: ";
+	ePoint kA = pA * nB;
+	ePoint::print_point(kA);
 	cout << endl;
+	cout << "Bob calculates common secret key: ";
+	ePoint kB = pB * nA;
+	ePoint::print_point(kB);
+	cout << endl << endl;
 
-	// int KA_x, KA_y, KB_x, KB_y;
-
-	// multi_point(KA_x, KA_y, pA_x, pA_y, a, n, nB);
-	// multi_point(KB_x, KB_y, pB_x, pB_y, a, n, nB);
-
-	// cout << "Alice sends Bob her public key. Bob does the same." << endl;
-	// cout << "Alice calculates common secret key: (" << KA_x << ", " << KA_y << ")" << endl;
-	// cout << "Bob calculates common secret key: (" << KB_x << ", " << KB_y << ")" << endl << endl;
-
-	// if (KA_x == KB_x && KA_y == KB_y)
-	// 	cout << "Keys are the same. Success!" << endl;
-	// else
-	// 	cout << "Keys are the different. Wrong!" << endl;
+	 if (kA == kB)
+	 	cout << "Keys are the same. Success!" << endl;
+	 else
+	 	cout << "Keys are the different. Wrong!" << endl;
 
 
 	system("pause");
